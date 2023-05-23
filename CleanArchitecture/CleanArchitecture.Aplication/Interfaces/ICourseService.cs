5d14f9dbe55f3904c79e2890e7be409d17c2052e
Oleksandr Prokopets <prokopets.alexandr@gmail.com>
2023-05-23 22:52:57 +0200

    Add Service and Repository interfaces, Add CourseVIewModel

---
+++ CleanArchitecture/CleanArchitecture.Aplication/Interfaces/ICourseService.cs
@@ -0,0 +1,10 @@
+﻿using CleanArchitecture.Aplication.ViewModels;
+using System.Collections.Generic;
+
+namespace CleanArchitecture.Aplication.Interfaces
+{
+    public interface ICourseService
+    {
+        IReadOnlyCollection<CourseViewModel> GetCourses(); 
+    }
+}
---
+++ CleanArchitecture/CleanArchitecture.Aplication/ViewModels/CourseViewModel.cs
@@ -0,0 +1,10 @@
+﻿using CleanArchitecture.Domain.Models;
+using System.Collections.Generic;
+
+namespace CleanArchitecture.Aplication.ViewModels
+{
+    public class CourseViewModel
+    {
+        public List<Course> Courses { get; set; }
+    }
+}
