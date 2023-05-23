ae321534ca83e14e8f350ebd6c104613f62bc956
Oleksandr Prokopets <prokopets.alexandr@gmail.com>
2023-05-23 22:39:05 +0200

    Initial commit of Infrastructure.Data project, update MVC for new UniversityDbContext

--- CleanArchitecture/CleanArchitecture.Mvc/Startup.cs
+++ CleanArchitecture/CleanArchitecture.Mvc/Startup.cs
@@ -1,17 +1,12 @@
+using CleanArchitecture.Infrastructure.Data.Context;
 using CleanArchitecture.Mvc.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace CleanArchitecture.Mvc
 {
@@ -29,7 +24,13 @@ namespace CleanArchitecture.Mvc
         {
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
-                    Configuration.GetConnectionString("UniversityIdentityDBConnection")));
+                    Configuration.GetConnectionString("UniversityIdentityDbConnection")));
+
+            services.AddDbContext<UniversityDbContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("UniversityDbConnection"));
+            });
+
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
