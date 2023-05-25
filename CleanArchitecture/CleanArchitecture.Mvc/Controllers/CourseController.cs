584c8c4092ffab6a9a293d9254951cf8c1a51339
Oleksandr Prokopets <prokopets.alexandr@gmail.com>
2023-05-25 22:35:15 +0200

    Commit changes for Presentation course controller, views, styling and authentication

---
+++ CleanArchitecture/CleanArchitecture.Mvc/Controllers/CourseController.cs
@@ -0,0 +1,24 @@
+﻿using CleanArchitecture.Aplication.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArchitecture.Mvc.Controllers
+{
+    [Authorize]
+    public class CourseController : Controller
+    {
+        private readonly ICourseService _courseService;
+
+        public CourseController(ICourseService courseService)
+        {
+            _courseService = courseService;
+        }
+
+        public IActionResult Index()
+        {
+            var model = _courseService.GetCourses();
+
+            return View(model);
+        }
+    }
+}
